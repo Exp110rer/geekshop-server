d8475f37a60d9e8ed15d61c8d0c6b99d07268f97
Exp110rer <a_sakharov@mail.ru>
2021-09-09 01:27:06 +0300

    Completed Lesson6.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-import json
-import os
+# import json
+# import os
 from .models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -11,9 +12,21 @@ def index(request):
     return render(request, 'products/index.html', context=data)
 
 
-def products(request):
+def products(request, category_id=None, page=1):
     data = dict()
     data['title'] = 'GeekPRODUCTS'
     data['category_list'] = ProductCategory.objects.all()
-    data['product_list'] = Product.objects.all()
+    # data['product_list'] = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    products_list = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    paginator = Paginator(products_list, per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    data['product_list'] = products_paginator
     return render(request, 'products/products.html', context=data)
+
+
